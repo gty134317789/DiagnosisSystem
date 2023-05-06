8a05fb8615b78f12b24b4e3274772ff311ba1709
P.E.E.K.A <gutianyuan33@126.com>
2023-05-06 23:55:50 +0800

    1.完成了训练模型所有的表单验证； 2.明天完成后端的传参。

--- FrontEnd/DiagnosisSystem/src/components/Train.vue
+++ FrontEnd/DiagnosisSystem/src/components/Train.vue
@@ -1,51 +1,38 @@
 <template>
     <div style="margin-top: 60px;margin-left:80px;width: 600px" >
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" >
-            <el-form-item label="样本类别数量" prop="num_classes"
-                          >
+            <el-form-item label="样本类别数量" prop="num_classes">
                 <el-input v-model.number="ruleForm.num_classes"></el-input>
             </el-form-item>
 
-            <el-form-item label="迭代次数" prop="epochs"
-                          :rules="[
-    { required: true, message: '迭代次数不能为空'},
-  ]">
-                <el-input v-model="ruleForm.epochs"></el-input>
+            <el-form-item label="迭代次数" prop="epochs">
+                <el-input v-model.number="ruleForm.epochs"></el-input>
             </el-form-item>
 
-            <el-form-item label="每类样本数量" prop="number"
-                          :rules="[
-    { required: true, message: '地区不能为空'},
-  ]">
-                <el-input v-model="ruleForm.number"></el-input>
+            <el-form-item label="每类样本数量" prop="number">
+                <el-input v-model.number="ruleForm.number"></el-input>
             </el-form-item>
 
-            <el-form-item label="训练集比例" prop="train"
-                          :rules="[
-    { required: true, message: '联系方式不能为空'},
-  ]">
-                <el-input v-model="ruleForm.train"></el-input>
-             </el-form-item>
-
-            <el-form-item label="验证集比例" prop="valid"
-                          :rules="[
-    { required: true, message: '简介不能为空'},
-  ]">
-                <el-input v-model="ruleForm.valid"></el-input>
+            <el-form-item label="训练集比例" prop="train">
+                <el-input-number v-model.number="ruleForm.train" :precision="2" :step="0.01" :max="1" :min="0"></el-input-number>
             </el-form-item>
 
-            <el-form-item label="测试集比例" prop="test"
-                          :rules="[
-    { required: true, message: '是否选中不能为空'},
-  ]">
-                <el-input v-model="ruleForm.test"></el-input>
-            </el-form-item>
+           <el-form-item label="验证集比例" prop="valid">
+                <el-input-number v-model.number="ruleForm.valid" :precision="2" :step="0.01" :max="1" :min="0"></el-input-number>
+           </el-form-item>
 
+          <el-form-item label="测试集比例" prop="test">
+                <el-input-number v-model.number="ruleForm.test" :precision="2" :step="0.01" :max="1" :min="0"></el-input-number>
+          </el-form-item>
 
-            <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">确定</el-button>
-                <el-button @click="resetForm('ruleForm')">重置</el-button>
-            </el-form-item>
+          <el-form-item>
+           <el-button type="primary" @click="checkForm('ruleForm')">校验</el-button>
+          </el-form-item>
+
+          <el-form-item>
+            <el-button type="primary" v-bind:disabled="ruleForm.check" @click="submitForm('ruleForm')">确定</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
         </el-form>
     </div>
 </template>
@@ -60,22 +47,61 @@ const makdir='http://127.0.0.1:5001/makeDatadir';
     export default {
         name: "Train",
         data() {
+          //验证样本数量
           var checkNum = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('样本类别数量不能为空'));
-        }
-        setTimeout(() => {
-          if (!Number.isInteger(value)) {
-            callback(new Error('请输入数字值'));
-          } else {
-            if (value>11 || value<0) {
-              callback(new Error('样本类别数量必须在1-10'));
-            } else {
-              callback();
-            }
+          if (!value) {
+            return callback(new Error('样本类别数量不能为空'));
           }
-        }, 1000);
-      };
+          setTimeout(() => {
+            if (!Number.isInteger(value)) {
+              callback(new Error('请输入数字值'));
+            } else {
+              if (value>10 || value<0) {
+                callback(new Error('样本类别数量必须在1-10'));
+              } else {
+                callback();
+              }
+            }
+          }, 100);
+        };
+
+          //验证迭代次数
+           var checkEpochs = (rule, value, callback) => {
+          if (!value) {
+            return callback(new Error('每类样本数量不能为空'));
+          }
+          setTimeout(() => {
+            if (!Number.isInteger(value)) {
+              callback(new Error('请输入数字值'));
+            } else {
+              if (value>200 || value<100) {
+                callback(new Error('迭代次数需要在100-200次之间'));
+              } else {
+                callback();
+              }
+            }
+          }, 100);
+        };
+
+           //验证每类样本数量
+           var checkNumbers = (rule, value, callback) => {
+          if (!value) {
+            return callback(new Error('每类样本数量不能为空'));
+          }
+          setTimeout(() => {
+            if (!Number.isInteger(value)) {
+              callback(new Error('请输入数字值'));
+            } else {
+              if (value>784 || value<100) {
+                callback(new Error('每类样本数量需要在100-784个之间'));
+              } else {
+                callback();
+              }
+            }
+          }, 100);
+        };
+
+
             return {
               fileList:[],
                 ruleForm: {
@@ -84,13 +110,32 @@ const makdir='http://127.0.0.1:5001/makeDatadir';
                     number: '',
                     train: '',
                     valid:'',
-                    test:''
+                    test:'',
+                    check:true
                 },
               rules:{
                 num_classes:[
                    { required: true, message: '样本类别数量不能为空'},
                   { validator: checkNum, trigger: 'blur' }
-                ]
+                ],
+                epochs:[
+                  { required: true, message: '迭代次数不能为空'},
+                  { validator: checkEpochs, trigger: 'blur' }
+                ],
+                number:[
+                  { required: true, message: '每类样本数量不能为空'},
+                  { validator: checkNumbers, trigger: 'blur' }
+                ],
+                train:[
+                  { required: true, message: '训练集比例不能为空'},
+                ],
+                valid:[
+                  { required: true, message: '验证集比例不能为空'},
+                ],
+                test:[
+                  { required: true, message: '测试集比例不能为空'},
+                ],
+
               }
             };
         },
@@ -111,12 +156,32 @@ const makdir='http://127.0.0.1:5001/makeDatadir';
 
             },
             resetForm(formName) {
-                const that=this
+                const that=this;
                 that.$refs[formName].resetFields();
             },
+            checkForm(formName){
+              const that=this;
+              if(that.ruleForm.train+that.ruleForm.valid+that.ruleForm.test==1){
+                that.$alert('数据无误可提交', '', {
+                                    confirmButtonText: '确定',
+                                    callback: action => {this.ruleForm.check=false}
+                                });
+              }
+              else if(that.ruleForm.train+that.ruleForm.valid+that.ruleForm.test<1){
+                 that.$alert('数据有误，训练集、验证集、测试集和小于1', '', {
+                                    confirmButtonText: '确定',
+                                    callback: action => {this.ruleForm.check=true}
+                                });
+              }
+              else if(that.ruleForm.train+that.ruleForm.valid+that.ruleForm.test>1){
+                 that.$alert('数据有误，训练集、验证集、测试集和大于1', '', {
+                                    confirmButtonText: '确定',
+                                    callback: action => {this.ruleForm.check=true}
+                                });
+              }
+            }
+  },
 
-
-        }
 
 }
 
