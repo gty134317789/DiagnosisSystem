49a41daba608ecb1a42c2a3d337f61f9a1d213ed
P.E.E.K.A <gutianyuan33@126.com>
2023-05-23 22:57:40 +0800

    1.开发完成本体。

---
+++ FrontEnd/DiagnosisSystem/src/components/Help.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h1>问题反馈</h1>
+  <pre>{{ fileContent }}</pre>
+</div>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      fileContent: 'QQ:123456789 ' +
+        '手机号:1234567890',
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
